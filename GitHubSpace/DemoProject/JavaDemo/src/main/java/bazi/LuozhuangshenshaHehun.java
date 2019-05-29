f03852e37900c7ee2902f2b5c7b391b53e2a0c4f
szchengz <cgz@foxmail.com>
2019-05-29 23:10:16 +0800

    八字

--- GitHubSpace/DemoProject/JavaDemo/src/main/java/bazi/LuozhuangshenshaHehun.java
+++ GitHubSpace/DemoProject/JavaDemo/src/main/java/bazi/LuozhuangshenshaHehun.java
@@ -147,7 +147,7 @@ public class LuozhuangshenshaHehun {
 
     }
 
-    private Calendar getCalendarfromString(String year, String DateFormat) throws ParseException {
+    public Calendar getCalendarfromString(String year, String DateFormat) throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat(DateFormat);
         Calendar cal = Calendar.getInstance();
         cal.setTime(sdf.parse(year));
